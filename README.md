6d478025a71f7c9ef64b001ffa59e7be06b16e6d
root <root@LAPTOP-CIIBPF5B.localdomain>
2020-11-03 18:43:08 +0100

    preparing the project

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # SpringCMS
+
+Zadanie 1
+Otwórz IDE a następnie utwórz w nim projekt o nazwie SpringCMS.
+Utwórz encje o nazwie Category.
+Encja ma zawierać następujące pola:
+id
+name (max 100 znaków)
+description (może przyjmować wartość null)
+
+
+Zadanie 2
+Utwórz encje o nazwie Author.
+Encja ma zawierać następujące pola:
+id
+firstName
+lastName
+
+
+Zadanie 3
+Utwórz encję Article,
+Encja ma zawierać następujące pola:
+id
+title (max. 200 znaków),
+author - (powiązanie relacją do klasy Author) - artykuł może mieć tylko jednego autora
+categories - (powiązanie relacją do klasy Category) - artykuł może należeć do wielu kategorii
+content
+created (wartość ma być automatycznie dodawana podczas zapisu)
+updated (wartość ma być automatycznie zmieniana podczas edycji).
+
+
+Zadanie 4
+Uzupełnij ręcznie dane w bazie, lub stwórz odpowiedni skrypt, który zostanie załadowany podczas startu aplikacji.
+Utwórz klasy ArticleDao, AuthorDao, CategoryDao.
+W klasach Dao utwórz metody pozwalające na:
+zapis encji
+usuwanie encji
+edycję encji
+
+
---
+++ src/main/java/pl/coderslab/app/AppConfig.java
@@ -0,0 +1,68 @@
+package pl.coderslab.app;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//import pl.coderslab.app.converter.PublisherConverter;
+
+import javax.persistence.EntityManagerFactory;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("pl.coderslab")
+@EnableTransactionManagement
+public class AppConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        StringHttpMessageConverter stringConverter = new StringHttpMessageConverter();
+        stringConverter.setSupportedMediaTypes(Arrays.asList(new MediaType("text", "plain", StandardCharsets.UTF_8)));
+        converters.add(stringConverter);
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/WEB-INF/views/", ".jsp");
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+//    @Override
+//    public void addFormatters(FormatterRegistry registry) {
+//        registry.addConverter(publisherConverter());
+//    }
+
+    @Bean
+    public LocalEntityManagerFactoryBean entityManagerFactory() {
+        LocalEntityManagerFactoryBean entityManagerFactoryBean = new LocalEntityManagerFactoryBean();
+        entityManagerFactoryBean.setPersistenceUnitName("PersistenceUnit");
+        return entityManagerFactoryBean;
+    }
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager jpaTransactionManager = new JpaTransactionManager(entityManagerFactory);
+        return jpaTransactionManager;
+    }
+
+//    @Bean
+//    public PublisherConverter publisherConverter() {
+//        return new PublisherConverter();
+//    }
+}
